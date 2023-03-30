d7e8b2e632f3723d42cd252dd61f204974dc371c
Eduard Heijkoop <Eduard.Heijkoop@gmail.com>
2023-03-30 15:45:25 -0600

    Cleaned up comments

--- gcp_utils.py
+++ gcp_utils.py
@@ -1,23 +1,19 @@
 import numpy as np
 import h5py
-from icesat2_utils import gps2utc
-
 
 def analyze_icesat2_land(icesat2_dir,df_city,shp_data,beam_flag=False):
-    #Given a directory of downloaded ATL03 hdf5 files,
-    #reads them and writes the high confidence photons to a CSV as:
-    #longitude,latitude,height (WGS84),time [UTC]
+    '''
+    Given a directory of downloaded ATL03 hdf5 files,
+    reads them and writes the high confidence photons to a CSV as:
+    longitude,latitude,height [WGS84],time [UTC](,beam)
+    '''
     city_name = df_city.city
     icesat2_list = icesat2_dir+city_name + '/icesat2_list.txt'
     with open(icesat2_list) as f3:
         file_list = f3.read().splitlines()
     beam_list_r = ['gt1r','gt2r','gt3r']
     beam_list_l = ['gt1l','gt2l','gt3l']
-    #Initialize arrays and start reading .h5 files
-    # lon = np.empty([0,1],dtype=float)
-    # lat = np.empty([0,1],dtype=float)
-    # h = np.empty([0,1],dtype=float)
-    lon_high_conf = np.empty([0,1],dtype=float)
+    lon_high_conf = np.empty([0,1],dtype=float) #Initialize arrays and start reading .h5 files
     lat_high_conf = np.empty([0,1],dtype=float)
     h_high_conf = np.empty([0,1],dtype=float)
     delta_time_total_high_conf = np.empty([0,1],dtype=float)
@@ -27,9 +23,7 @@ def analyze_icesat2_land(icesat2_dir,df_city,shp_data,beam_flag=False):
         full_file = icesat2_dir + city_name + '/' + h5_file
         atl03_file = h5py.File(full_file,'r')
         list(atl03_file.keys())
-        sc_orient = atl03_file['/orbit_info/sc_orient']
-        sc_orient = sc_orient[0]
-        #Select strong beams according to S/C orientation
+        sc_orient = atl03_file['/orbit_info/sc_orient'][0] #Select strong beams according to S/C orientation
         if sc_orient == 1:
             beam_list_req = beam_list_r
         elif sc_orient == 0:
@@ -37,8 +31,10 @@ def analyze_icesat2_land(icesat2_dir,df_city,shp_data,beam_flag=False):
         elif sc_orient == 2:
             continue
         for beam in beam_list_req:
-            #Some beams don't actually have any height data in them, so this is done to skip those
-            #Sometimes only one or two beams are present, this also prevents looking for those
+            '''
+            Some beams don't actually have any height data in them, so this is done to skip those
+            Sometimes only one or two beams are present, this also prevents looking for those
+            '''
             heights_check = False
             heights_check = '/'+beam+'/heights' in atl03_file
             if heights_check == False:
@@ -51,8 +47,7 @@ def analyze_icesat2_land(icesat2_dir,df_city,shp_data,beam_flag=False):
             tmp_delta_time_total = tmp_sdp + tmp_delta_time
             tmp_signal_conf = np.asarray(atl03_file['/'+beam+'/heights/signal_conf_ph'])
             tmp_high_conf = tmp_signal_conf[:,0] == 4
-            #If fewer than 100 high confidence photons are in an hdf5 file, skip
-            if len(tmp_high_conf) < 100:
+            if len(tmp_high_conf) < 100: #If fewer than 100 high confidence photons are in an hdf5 file, skip
                 continue
             tmp_ph_index_beg = np.asarray(atl03_file['/'+beam+'/geolocation/ph_index_beg']).squeeze()
             tmp_ph_index_beg = tmp_ph_index_beg - 1
@@ -60,8 +55,7 @@ def analyze_icesat2_land(icesat2_dir,df_city,shp_data,beam_flag=False):
             tmp_ref_ph_index = np.asarray(atl03_file['/'+beam+'/geolocation/reference_photon_index']).squeeze()
             tmp_ph_index_end = tmp_ph_index_beg + tmp_segment_ph_cnt
             tmp_podppd_flag = np.asarray(atl03_file['/'+beam+'/geolocation/podppd_flag']).squeeze()
-            #no valid photons to "create" a ref photon -> revert back to reference ground track, which we don't want, so select segments with >0 photons
-            idx_ref_ph = tmp_segment_ph_cnt>0
+            idx_ref_ph = tmp_segment_ph_cnt>0 #no valid photons to "create" a ref photon -> revert back to reference ground track, which we don't want, so select segments with >0 photons
             tmp_ph_index_beg = tmp_ph_index_beg[idx_ref_ph]
             tmp_ph_index_end = tmp_ph_index_end[idx_ref_ph]
             tmp_segment_ph_cnt = tmp_segment_ph_cnt[idx_ref_ph]
@@ -71,10 +65,8 @@ def analyze_icesat2_land(icesat2_dir,df_city,shp_data,beam_flag=False):
             for i in range(len(tmp_ph_index_beg)):
                 tmp_podppd_flag_full_ph[tmp_ph_index_beg[i]:tmp_ph_index_end[i]] = tmp_podppd_flag[i]
             tmp_idx_podppd = tmp_podppd_flag_full_ph == 0
-
             idx_nan = np.isnan(tmp_h)
             idx_flags = np.all((tmp_high_conf,tmp_idx_podppd,~idx_nan),axis=0)
-
             tmp_lon_high_conf = tmp_lon[idx_flags]
             tmp_lat_high_conf = tmp_lat[idx_flags]
             tmp_h_high_conf = tmp_h[idx_flags]
@@ -86,8 +78,11 @@ def analyze_icesat2_land(icesat2_dir,df_city,shp_data,beam_flag=False):
             if beam_flag == True:
                 tmp_beam_high_conf = np.repeat(beam,len(tmp_lon_high_conf))
                 beam_high_conf = np.append(beam_high_conf,tmp_beam_high_conf)
-    #A lot of data will be captured off the coast that we don't want, this is a quick way of getting rid of that
-    #Also prevents areas with no SRTM from being queried
+    '''
+    A lot of data will be captured off the coast that we don't want,
+    this is a quick way of getting rid of that
+    Also prevents areas with no SRTM from being queried
+    '''
     idx_lon = np.logical_or(lon_high_conf < np.min(shp_data.bounds.minx),lon_high_conf > np.max(shp_data.bounds.maxx))
     idx_lat = np.logical_or(lat_high_conf < np.min(shp_data.bounds.miny),lat_high_conf > np.max(shp_data.bounds.maxy))
     idx_tot = np.logical_or(idx_lon,idx_lat)
--- ocean_utils.py
+++ ocean_utils.py
@@ -4,8 +4,8 @@ import h5py
 from icesat2_utils import gps2utc,great_circle_distance
 import scipy
 
-from sklearn.cluster import DBSCAN
-from sklearn.preprocessing import StandardScaler
+# from sklearn.cluster import DBSCAN
+# from sklearn.preprocessing import StandardScaler
 
 import pyTMD.time
 import pyTMD.model
@@ -14,23 +14,18 @@ from pyTMD.infer_minor_corrections import infer_minor_corrections
 from pyTMD.predict_tidal_ts import predict_tidal_ts
 from pyTMD.read_FES_model import extract_FES_constants
 
-
-total_seconds = np.vectorize(datetime.timedelta.total_seconds)
-
 def analyze_icesat2_ocean(icesat2_dir,df_city,model_dir,geophys_corr_toggle=True,ocean_tide_replacement_toggle=False,extrapolate_fes2014=True,beam_flag=False):
-    #Given a directory of downloaded ATL03 hdf5 files,
-    #reads them and writes the high confidence photons to a CSV as:
-    #longitude,latitude,height (WGS84),time [UTC]
+    '''
+    Given a directory of downloaded ATL03 hdf5 files,
+    reads them and writes the high confidence photons to a CSV as:
+    longitude,latitude,height [WGS84],time [UTC](,beam)
+    '''
     city_name = df_city.city
     icesat2_list = icesat2_dir+city_name + '/icesat2_list.txt'
     with open(icesat2_list) as f3:
         file_list = f3.read().splitlines()
     beam_list_r = ['gt1r','gt2r','gt3r']
     beam_list_l = ['gt1l','gt2l','gt3l']
-    #Initialize arrays and start reading .h5 files
-    #lon = np.empty([0,1],dtype=float)
-    #lat = np.empty([0,1],dtype=float)
-    #h = np.empty([0,1],dtype=float)
     lon_high_med_conf = np.empty([0,1],dtype=float)
     lat_high_med_conf = np.empty([0,1],dtype=float)
     h_high_med_conf = np.empty([0,1],dtype=float)
@@ -40,9 +35,7 @@ def analyze_icesat2_ocean(icesat2_dir,df_city,model_dir,geophys_corr_toggle=True
     for h5_file in file_list:
         full_file = icesat2_dir + city_name + '/' + h5_file
         atl03_file = h5py.File(full_file,'r')
-        #list(atl03_file.keys())
-        sc_orient = atl03_file['/orbit_info/sc_orient'][0]
-        #Select strong beams according to S/C orientation
+        sc_orient = atl03_file['/orbit_info/sc_orient'][0] #Select strong beams according to S/C orientation
         if sc_orient == 1:
             beam_list_req = beam_list_r
         elif sc_orient == 0:
@@ -50,14 +43,15 @@ def analyze_icesat2_ocean(icesat2_dir,df_city,model_dir,geophys_corr_toggle=True
         elif sc_orient == 2:
             continue
         for beam in beam_list_req:
-            #Some beams don't actually have any height data in them, so this is done to skip those
-            #Sometimes only one or two beams are present, this also prevents looking for those
+            '''         
+            Some beams don't actually have any height data in them, so this is done to skip those
+            Sometimes only one or two beams are present, this also prevents looking for those
+            '''
             heights_check = False
             heights_check = '/'+beam+'/heights' in atl03_file
             if heights_check == False:
                 continue
             tmp_sdp = np.asarray(atl03_file['/ancillary_data/atlas_sdp_gps_epoch']).squeeze()
-            #full photon rate
             tmp_lon = np.asarray(atl03_file['/'+beam+'/heights/lon_ph']).squeeze()
             tmp_lat = np.asarray(atl03_file['/'+beam+'/heights/lat_ph']).squeeze()
             tmp_h = np.asarray(atl03_file['/'+beam+'/heights/h_ph']).squeeze()
@@ -65,10 +59,8 @@ def analyze_icesat2_ocean(icesat2_dir,df_city,model_dir,geophys_corr_toggle=True
             tmp_delta_time_total = tmp_sdp + tmp_delta_time
             tmp_signal_conf = np.asarray(atl03_file['/'+beam+'/heights/signal_conf_ph'])
             tmp_high_med_conf = np.logical_or(tmp_signal_conf[:,1]==3,tmp_signal_conf[:,1]==4)
-            #If fewer than 100 high/medium confidence photons are in an hdf5 file, skip it
-            if np.sum(tmp_high_med_conf) < 100:
+            if np.sum(tmp_high_med_conf) < 100: #If fewer than 100 high/medium confidence photons are in an hdf5 file, skip it
                 continue
-            #reference photon rate
             tmp_lon_ref = np.asarray(atl03_file['/'+beam+'/geolocation/reference_photon_lon']).squeeze()
             tmp_lat_ref = np.asarray(atl03_file['/'+beam+'/geolocation/reference_photon_lat']).squeeze()
             tmp_delta_time_geophys_corr = np.asarray(atl03_file['/'+beam+'/geophys_corr/delta_time']).squeeze()
@@ -82,9 +74,7 @@ def analyze_icesat2_ocean(icesat2_dir,df_city,model_dir,geophys_corr_toggle=True
             tmp_dac = np.asarray(atl03_file['/'+beam+'/geophys_corr/dac']).squeeze()
             tmp_eq_tide = np.asarray(atl03_file['/'+beam+'/geophys_corr/tide_equilibrium']).squeeze()
             tmp_podppd_flag = np.asarray(atl03_file['/'+beam+'/geolocation/podppd_flag']).squeeze()
-            #no valid photons to "create" a ref photon -> revert back to reference ground track, which we don't want, so select segments with >0 photons
-            idx_ref_ph = tmp_segment_ph_cnt>0
-
+            idx_ref_ph = tmp_segment_ph_cnt>0 #no valid photons to "create" a ref photon -> revert back to reference ground track, which we don't want, so select segments with >0 photons
             tmp_lon_ref = tmp_lon_ref[idx_ref_ph]
             tmp_lat_ref = tmp_lat_ref[idx_ref_ph]
             tmp_delta_time_total_geophys_corr = tmp_delta_time_total_geophys_corr[idx_ref_ph]
@@ -100,17 +90,16 @@ def analyze_icesat2_ocean(icesat2_dir,df_city,model_dir,geophys_corr_toggle=True
             for i in range(len(tmp_ph_index_beg)):
                 tmp_podppd_flag_full_ph[tmp_ph_index_beg[i]:tmp_ph_index_end[i]] = tmp_podppd_flag[i]
             tmp_idx_podppd = tmp_podppd_flag_full_ph == 0
-
-
             if geophys_corr_toggle == True:
-                #even after ref photon filtering, still areas with no tides (due to 0.5x0.5deg resolution)
-                #also no equilibrium tides there, if we are going to apply geophysical corrections without
-                #tide model swapping, skip the areas with no tides
-
-                #not all photons associated with a particular reference photon have the same signal confidences,
-                #i.e. do the high/med conf filtering after all the reference photon rate analysis has been done
-                #to prevent indexing problems
-                #Note: by convention we *subtract* the corrections to apply them
+                '''
+                even after ref photon filtering, still areas with no tides (due to 0.5x0.5deg resolution)
+                also no equilibrium tides there, if we are going to apply geophysical corrections without
+                tide model swapping, skip the areas with no tides
+                Not all photons associated with a particular reference photon have the same signal confidences,
+                i.e. do the high/med conf filtering after all the reference photon rate analysis has been done
+                to prevent indexing problems
+                Note: by convention we *subtract* the corrections to apply them
+                '''
                 if ocean_tide_replacement_toggle == False:
                     idx_no_got_tides = tmp_ocean_tide > 1e38
                     for i in np.atleast_1d(np.argwhere(idx_no_got_tides==False).squeeze()):
@@ -131,20 +120,19 @@ def analyze_icesat2_ocean(icesat2_dir,df_city,model_dir,geophys_corr_toggle=True
                     fes_interp = interp_func(dist_ref_ph)
                     for i in range(len(tmp_ref_ph_index)):
                         tmp_h[tmp_ph_index_beg[i]:tmp_ph_index_end[i]] -= (fes_interp[i] + tmp_dac[i])
-
-                    # for i in np.atleast_1d(np.argwhere(idx_no_fes_tides==False).squeeze()):
-                    #     tmp_h[tmp_ph_index_beg[i]:tmp_ph_index_end[i]] -= (fes2014_heights[i] + tmp_dac[i])
-                    # for i in np.atleast_1d(np.argwhere(idx_no_fes_tides).squeeze()):
-                    #     tmp_h[tmp_ph_index_beg[i]:tmp_ph_index_end[i]] = np.nan
-
+                    '''
+                    Old method:
+                    for i in np.atleast_1d(np.argwhere(idx_no_fes_tides==False).squeeze()):
+                        tmp_h[tmp_ph_index_beg[i]:tmp_ph_index_end[i]] -= (fes2014_heights[i] + tmp_dac[i])
+                    for i in np.atleast_1d(np.argwhere(idx_no_fes_tides).squeeze()):
+                        tmp_h[tmp_ph_index_beg[i]:tmp_ph_index_end[i]] = np.nan
+                    '''
             idx_nan = np.isnan(tmp_h)
             idx_flags = np.all((tmp_high_med_conf,tmp_idx_podppd,~idx_nan),axis=0)
-
             tmp_lon_high_med_conf = tmp_lon[idx_flags]
             tmp_lat_high_med_conf = tmp_lat[idx_flags]
             tmp_h_high_med_conf = tmp_h[idx_flags]
             tmp_delta_time_total_high_med_conf = tmp_delta_time_total[idx_flags]
-
             lon_high_med_conf = np.append(lon_high_med_conf,tmp_lon_high_med_conf)
             lat_high_med_conf = np.append(lat_high_med_conf,tmp_lat_high_med_conf)
             h_high_med_conf = np.append(h_high_med_conf,tmp_h_high_med_conf)
@@ -198,12 +186,12 @@ def ocean_tide_replacement(lon,lat,utc_time,model_dir):
         tide_heights[idx_unique_date] = tmp_tide_heights
     return tide_heights
 
-def cluster_photon_cloud(lon,lat,time,height,clustering_method='DBSCAN'):
-    '''
-    Clusters photon cloud with either DBSCAN or 3sigma along-track
-    '''
-    if clustering_method == 'DBSCAN':
+# def cluster_photon_cloud(lon,lat,time,height,clustering_method='DBSCAN'):
+#     '''
+#     Clusters photon cloud with either DBSCAN or 3sigma along-track
+#     '''
+#     if clustering_method == 'DBSCAN':
         
-        return None
-    elif clustering_method == '3sigma':
-        return None
+#         return None
+#     elif clustering_method == '3sigma':
+#         return None
